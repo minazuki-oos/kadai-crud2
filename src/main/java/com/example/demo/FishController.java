8099c57eb3f991195163d88e84c9ddf078f385ea
minazuki <aratanotennis@gmail.com>
2024-01-06 18:34:23 +0900

    feat: POSTリクエスト時にLocationヘッダーを追加

--- src/main/java/com/example/demo/FishController.java
+++ src/main/java/com/example/demo/FishController.java
@@ -1,6 +1,7 @@
 package com.example.demo;// FishController.java
 
-import org.springframework.http.HttpStatus;
+import java.net.URI;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/fish_database")
@@ -25,16 +27,28 @@ public class FishController {
   }
 
   @PostMapping
-  public ResponseEntity<Fish> createFish(@RequestBody Fish fish) {
+  public ResponseEntity<Fish> createFish(@RequestBody Fish fish, UriComponentsBuilder uriBuilder) {
     Fish createdFish = fishService.createFish(fish);
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdFish);
+
+    // 新しい魚のURIを構築
+    URI location = uriBuilder.path("/fish_database/{id}")
+        .buildAndExpand(createdFish.getId())
+        .toUri();
+
+    // HTTPステータス201 (Created) とともに新しい魚の情報を含むレスポンスを返す
+    return ResponseEntity.created(location).body(createdFish);
   }
 
   @GetMapping("/{id}")
-  public Fish getFishById(@PathVariable Long id) {
-    return fishService.getFishById(id);
+  public ResponseEntity<Fish> getFishById(
+      @PathVariable Long id) {  // Fish を ResponseEntity<Fish> に変更
+    Fish fish = fishService.getFishById(id);
+    return ResponseEntity.ok(fish);
   }
 
-
+  @GetMapping("/all")
+  public ResponseEntity<List<Fish>> getAllFish() {
+    List<Fish> allFish = fishService.getAllFish();
+    return ResponseEntity.ok(allFish);
+  }
 }
-
